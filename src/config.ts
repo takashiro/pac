e3b4ec69d4eb4cee33f5cd2e26f9f4744e5ed2bf
Kazuichi Takashiro <takashiro@qq.com>
2021-11-13 09:58:51 +0800

    Add more websites

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,19 @@
 export const protectedHosts: string[] = [
 	'google.com',
+	'googleapis.com',
+	'googleusercontent.com',
 	'github.com',
 	'tw',
+	'jp',
+	'digitaloceanspaces.com',
+	'gravatar.com',
+	'twitter.com',
+	'twimg.com',
+	'pixiv.net',
+	'youtube.com',
+	'ytimg.com',
+	'googlevideo.com',
+	'ggpht.com',
 ];
 
 export const proxy = 'PROXY localhost:1080;';
